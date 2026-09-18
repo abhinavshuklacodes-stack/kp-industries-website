a1a1ef34058c0808bbc1778168e7adad17f36753
abhinavshuklacodes-stack <abhinavshuklacodes@gmail.com>
2026-09-19 03:39:06 +0530

    initial script

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const menuOverlay = document.getElementById("menuOverlay");
+const menuLinks = document.querySelectorAll(".menu a");
 
+menuBtn.addEventListener("click", () => {
+    menuOverlay.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+    menuOverlay.classList.remove("open");
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menuOverlay.classList.remove("open");
+    });
+});
+
+document.getElementById("year").textContent =
+    new Date().getFullYear();
